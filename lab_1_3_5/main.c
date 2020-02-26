0639311ccedb3913078305a3a951b96d42531f8a
Shingarov Ilyas <ishingarov@gmail.com>
2020-02-26 23:13:14 +0300

    Task 5 initial commit

---
+++ lab_1_3_5/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int gcd(int a, int b);
+
+int main()
+{
+    int a, b;
+
+    printf("Input a:\n");
+    if (scanf("%d", &a) != 1 || a < 1)
+    {
+        printf("Incorrect input data");
+        return 1;    
+    }
+    
+    printf("Input b:\n");
+    if (scanf("%d", &b) != 1 || b < 1)
+    {
+        printf("Incorrect input data");
+        return 1;
+    }
+
+    int common_divisor = gcd(a, b);
+
+    printf("gcd: %d", common_divisor);
+
+    return 0;
+}
+
+int gcd(int a, int b)
+{
+    while (a != b)
+    {
+        if (a > b)
+            a -= b;
+        else
+            b -=a;
+    }
+
+    return a;
+}

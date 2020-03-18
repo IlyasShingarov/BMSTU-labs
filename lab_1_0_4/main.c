74bd7ee792b4598cd4432e4ec370abffe2aaa675
Shingarov Ilyas <ishingarov@gmail.com>
2020-03-18 22:05:18 +0300

    lab_1_0_4 loop removed

--- lab_1_0_4/main.c
+++ lab_1_0_4/main.c
@@ -10,41 +10,9 @@ int main(void)
     printf("Input an amount of money:\n");
     scanf("%d", &s);
 
-    int bottles, bottle_count;
-
-    bottle_count = 0;
-    
-    while (s >= 45)
-    {
-        bottles = buy(s);
-        bottle_count += bottles;
-        s -= bottles * 45;
-        s += sell(bottles);
-    }
+    int bottle_count = (s - 20) / 25;
 
     printf("Amount of bought bottles will be: %d", bottle_count);
 
     return 0;
-}
-
-int sell(int bottles)
-{
-    int s;
-
-    s = bottles * 20;
-
-    return s;
-}
-
-int buy(int s)
-{
-    int counter = 0;
-
-    while (s >= 45)
-    {
-        s -= 45;
-        counter += 1;
-    }
-    
-    return counter;
 }

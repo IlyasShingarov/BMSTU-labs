aed11b2e109337c830b041e702adaa33c2bd4a66
Shingarov Ilyas <ishingarov@gmail.com>
2020-03-22 17:41:16 +0300

    style fix

--- lab_2_0_4/main.c
+++ lab_2_0_4/main.c
@@ -39,10 +39,10 @@ void swap(long *xp, long *yp)
 
 void bubble_sort(long *a, int n)
 { 
-    for (int i = 0; i < n-1; i++)       
-        for (int j = 0; j < n-i-1; j++)  
-            if (a[j] > a[j+1]) 
-                swap(&a[j], &a[j+1]); 
+    for (int i = 0; i < n - 1; i++)       
+        for (int j = 0; j < n - i - 1; j++)  
+            if (a[j] > a[j + 1]) 
+                swap(&a[j], &a[j + 1]); 
 } 
 
 

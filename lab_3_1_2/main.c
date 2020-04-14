7204350680f32341010ab408f9ab611bdd463af8
Shingarov Ilyas <ishingarov@gmail.com>
2020-04-14 21:06:09 +0300

    Negative test fix

--- lab_3_1_2/main.c
+++ lab_3_1_2/main.c
@@ -100,17 +100,22 @@ int find_min_digit_sum(int (*mat)[MAX_COLS], int rows, int columns, int *min_i, 
 {   
     int min_sum = digit_sum(mat[0][0]);
 
+    int error = 0;
+
     for (int i = 0; i < rows; i++)
         for (int j = 0; j < columns; j++)
         {
             int sum = digit_sum(abs(mat[i][j]));
-            if (sum <= min_sum)
+            if (sum < min_sum)
             {
+                error = 0;
                 min_sum = sum;
                 *min_j = j;
                 *min_i = i;
             }
+            else if (sum == min_sum)
+                error = 1;
         }
 
-    return 0;
+    return error;
 }

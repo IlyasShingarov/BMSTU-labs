ce7d82168faeda81e738727f6d84a8c4aff0b4a9
Shingarov Ilyas <ishingarov@gmail.com>
2020-04-18 22:35:29 +0300

    task 5 initial commit

---
+++ lab_3_1_5/freq.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+#include "freq.h"
+
+int array_in(int *arr, int len)
+{
+    int error = 0;
+
+    printf("Input elements: ");
+    for (int i = 0; i < len && !error; i++)
+    {
+        error = scanf("%d", &arr[i]) != 1;
+    }
+    
+    return error;
+}
+
+int array_out(const int *array, int len)
+{    
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
+
+int mat_size_in(int *rows, int *columns)
+{
+    int error = 0;
+
+    printf("Input rows, columns: ");
+    if (scanf("%d%d", rows, columns) != 2 || *rows <= 0 || *rows > MAX_ROWS || *columns <= 0 || *columns > MAX_COLS)
+        error = 1;
+
+    return error;
+}
+
+int mat_in(int (*mat)[MAX_COLS], int rows, int columns)
+{
+    int error = 0;
+
+    for (int i = 0; i < rows && !error; i++)
+    {
+        error = array_in(mat[i], columns);
+    }
+
+    return error;
+}
+
+int mat_out(int (*matrix)[MAX_COLS], int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        array_out(matrix[i], cols);
+    }
+    
+    return 0;
+}
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+}
+
+int find_max(int *array, int n)
+{
+    int max = 0;
+
+    for (int *p = array; p < array + n; p++)
+    {
+        if (*p > max)
+            max = *p;
+    }
+
+    return max;
+}
---
+++ lab_3_1_5/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include "freq.h"
+
+#define SHIFT_COUNT 3
+#define MAX_ARRAY_LENGTH 100
+
+void shift_left(int *array, int len, int times);
+int form_array(int (*mat)[MAX_COLS], int rows, int columns, int *array);
+int digit_sum(int x);
+void return_elements(int (*mat)[MAX_COLS], int rows, int columns, int *array);
+
+
+int main(void)
+{
+    int error = 0;
+    int matrix[MAX_ROWS][MAX_COLS] = { { 0 } };
+    int array[MAX_ARRAY_LENGTH] = { 0 };
+    int rows = 0, columns = 0, n = 0;
+
+    error = mat_size_in(&rows, &columns);
+    if (!error)
+        error = mat_in(matrix, rows, columns);
+    
+    if (!error)
+        n = form_array(matrix, rows, columns, array);
+
+    if (!n)
+        error = 1;
+
+    if (!error)
+        {
+            shift_left(array, n, SHIFT_COUNT);
+            return_elements(matrix, rows, columns, array);
+        }
+
+    if (error)
+        printf("Error!");
+
+    if (!error)
+        mat_out(matrix, rows, columns);
+
+    return error;
+}
+
+void shift_left(int *array, int len, int times)
+{
+    for (int j = 0; j < times; j++)
+    {
+        int temp = array[0];
+        for (int i = 0; i < len - 1; i++)
+            array[i] = array[i + 1];
+        array[len - 1] = temp;
+    }
+}
+
+int form_array(int (*mat)[MAX_COLS], int rows, int columns, int *array)
+{
+    int n = 0;
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            if (digit_sum(mat[i][j]) > 10)
+            {
+                array[n] = mat[i][j];
+                n++;
+            }
+
+    return n;
+}
+
+int digit_sum(int x)
+{
+    int sum = 0;
+
+    while (x != 0)
+    {
+        sum += x % 10;
+        x = x / 10;
+    }
+
+    return sum;
+}
+
+void return_elements(int (*mat)[MAX_COLS], int rows, int columns, int *array)
+{
+    int counter = 0;
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            if (digit_sum(mat[i][j]) > 10)
+            {
+                mat[i][j] = array[counter];
+                counter++;
+            }
+}

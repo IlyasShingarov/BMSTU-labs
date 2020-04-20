be44ec456a02a9e9097c9600a846b2cf8646dc06
Shingarov Ilyas <ishingarov@gmail.com>
2020-04-20 11:34:05 +0300

    task 4 remade to **

--- lab_3_1_4/main.c
+++ lab_3_1_4/main.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include "freq.h"
 
-void swap_rows(int (*mat)[MAX_COLS], int rows, int columnns);
+void swap_rows(int **mat, int rows, int columnns);
 
 int main(void)
 {
     int error = 0;
 
-    int matrix[MAX_ROWS][MAX_COLS] = { { 0 } };
-    int rows, columns = 0;
+    int rows, columns;
+    int *matrix[MAX_ROWS];
+    int buffer[MAX_ROWS][MAX_COLS];
 
-    error = mat_size_in(&rows, &columns);
+    transform(matrix, *buffer, MAX_ROWS, MAX_COLS);
 
+    error = mat_in(matrix, &rows, &columns);
     if (rows != columns)
         error = 1;
-
-    if (!error)
-        error = mat_in(matrix, rows, columns);
 
     if (!error)
         swap_rows(matrix, rows, columns);
@@ -30,14 +29,14 @@ int main(void)
     return error;
 }
 
-void swap_rows(int (*mat)[MAX_COLS], int rows, int columnns)
+void swap_rows(int **mat, int rows, int columnns)
 {
     int counter = 0;
     for (int i = 0; i < rows / 2; i++)
     {
         for (int j = counter; j < columnns - counter; j++)
         {
-            swap(&mat[i][j], &mat[rows - i - 1][j]);
+            swap(*(mat + i) + j, *(mat + rows - i - 1) + j);
         }
         counter++;
     }   

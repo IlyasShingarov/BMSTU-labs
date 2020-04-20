a56dd3b59f584c0000739042295634f815bcb789
Shingarov Ilyas <ishingarov@gmail.com>
2020-04-20 10:56:41 +0300

    output space fix

--- lab_3_1_2/freq.c
+++ lab_3_1_2/freq.c
@@ -54,7 +54,7 @@ void mat_out(int **matrix, int rows, int cols)
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
-            printf("%d", matrix[i][j]);
+            printf("%d ", matrix[i][j]);
         printf("\n");
     }
 }

4259df7ac1357cb8ab4a2528e0a461bea3ba9854
Ilyas Shingarov <ishingarov@gmail.com>
2020-05-06 22:49:19 +0300

    style fix

--- lab_04_1/my_string.c
+++ lab_04_1/my_string.c
@@ -6,15 +6,16 @@
 
 
 
-size_t my_strspn(const char* string, const char* chars)
+size_t my_strspn(const char *string, const char *chars)
 {
 	size_t n;
 	const char *p;
 	for (n = 0; *string; string++, n++)
-	{	for (p = chars; *p && *p != *string; p++)
+	{
+		for (p = chars; *p && *p != *string; p++)
 			;
-	if (!*p)
-		break;
+		if (!*p)
+			break;
 	}
 
 	return n;

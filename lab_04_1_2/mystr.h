0984e3fc22a692dfdd29dd52934ed51c75b17c41
Ilyas Shingarov <ishingarov@gmail.com>
2020-05-10 01:58:14 +0300

    sort fix

--- lab_04_1_2/mystr.h
+++ lab_04_1_2/mystr.h
@@ -23,7 +23,7 @@ void remove_duplicates(char **words, size_t *word_count);
 
 void sort_words(char **words, size_t word_count);
 
-void swap(char *xp, char *yp);
+void swap(char **words, size_t ind1, size_t ind2);
 
 
 #endif //_MYSTR_H_

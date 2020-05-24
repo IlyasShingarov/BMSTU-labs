04490fb0fe2260db879049dd0ff1b2d370dab2d7
Ilyas Shingarov <ishingarov@gmail.com>
2020-05-24 15:52:10 +0300

    sort implemented

--- lab_51_3/functions.c
+++ lab_51_3/functions.c
@@ -48,7 +48,6 @@ int print(const char *dir)
             else
                 error = FILE_ERROR;
         }
-
     }
 
     if (file)
@@ -59,5 +58,65 @@ int print(const char *dir)
     else
         printf("Error");
 
+    return error;
+}
+
+int sort(const char *dir)
+{
+    int error = 0, file_size;
+
+    FILE *file = fopen(dir, "rb+");
+    if (!file)
+        error = NULL_PTR_ERROR;
+    
+    if (!(fseek(file, 0, SEEK_END)))
+        file_size = ftell(file);
+    
+    if (file_size < 1 || file_size % sizeof(int) != 0)
+        error = FILE_ERROR;
+
+    if (!error)
+    {
+        int elem1, elem2;
+        for (size_t i = 0; i < file_size / sizeof(int); i++)
+        {
+            for (size_t j = 0; j < (file_size / sizeof(int)) - 1 - i && !error; i++)
+            {
+                error = get_number_by_pos(file, j, &elem1);
+                if (!error)
+                    error = get_number_by_pos(file, j + 1, &elem2);
+
+                if (elem1 > elem2 && !error)
+                {
+                    error = put_number_by_pos(file, j, elem2);
+                    if (!error)
+                        error = put_number_by_pos(file, j + 1, elem1);
+                }
+            }
+        }    
+    }
+
+
+
+
+    if (file)
+        fclose(file);
+    
+    return error;
+}
+
+int get_number_by_pos(FILE *file, size_t pos, int *num)
+{
+    int error = fseek(file, pos * sizeof(int), SEEK_SET);
+    if (!error && fread(&num, sizeof(int), 1, file) != 1)
+        error = FILE_ERROR;
+    return error;
+}
+
+int put_number_by_pos(FILE *file, size_t pos, int num)
+{
+    int error = fseek(file, pos * sizeof(int), SEEK_SET);
+    if (!error && fwrite(&num, sizeof(int), 1, file) != 1)
+        error = FILE_ERROR;
     return error;
 }

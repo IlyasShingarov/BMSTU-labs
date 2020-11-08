c23b7752ec62627fa2e956b5b5df553210a4f37e
Shingarov Ilyas <ishingarov@gmail.com>
2020-11-08 20:34:01 +0300

    unit test fix

--- lab_08_06_01/unit_tests/check_normalization.c
+++ lab_08_06_01/unit_tests/check_normalization.c
@@ -9,15 +9,21 @@ START_TEST(test_delete_columns)
 {
     int rows = 2, columns = 4;
     int **matrix = allocate_matrix(rows, columns);
-    matrix[0][0] = 1, matrix[0][2] = 2, matrix[0][3] = 3, matrix[0][4] = 4;
-    matrix[1][0] = 5, matrix[1][2] = 6, matrix[1][3] = 7, matrix[1][4] = 8;
+    matrix[0][0] = 1, matrix[0][1] = 2, matrix[0][2] = 3, matrix[0][3] = 4;
+    matrix[1][0] = 5, matrix[1][1] = 6, matrix[1][2] = 7, matrix[1][3] = 8;
 
-    int matrix_res[2][2] = { {3, 4}, {7, 8} };
+    int **matrix_res = allocate_matrix(rows, rows);
+    matrix_res[0][0] = 3, matrix_res[0][1] = 4;
+    matrix_res[1][0] = 7, matrix_res[1][1] = 8;
 
     squarify_matrix(&matrix, &rows, &columns);
+
     for (int i = 0; i < rows; i++)
         for (int j = 0; j < columns; j++)
             ck_assert_int_eq(matrix[i][j], matrix_res[i][j]);
+
+    free_matrix(matrix_res, rows);
+    free_matrix(matrix, rows);
 }
 END_TEST
 

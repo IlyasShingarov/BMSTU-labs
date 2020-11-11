6311ec411003ffc0deb9c192f93ca7bba12486da
Shingarov Ilyas <ishingarov@gmail.com>
2020-11-12 00:59:02 +0300

    .-.

--- lab_08_06_01/unit_tests/check_normalization.c
+++ lab_08_06_01/unit_tests/check_normalization.c
@@ -78,14 +78,14 @@ START_TEST(test_equalize_b)
     cols_a = rows_a = size_a = 2;
     cols_b = rows_b = size_b = 3;
 
-    int **b = allocate_matrix(size_a, size_a);
-    b[0][0] = 1, b[0][1] = 2;
-    b[1][0] = 3, b[1][1] = 4;
+    int **a = allocate_matrix(size_a, size_a);
+    a[0][0] = 1, a[0][1] = 2;
+    a[1][0] = 3, a[1][1] = 4;
 
-    int **a = allocate_matrix(size_b, size_b);
-    a[0][0] = 1, a[0][1] = 2, a[0][2] = 3; 
-    a[1][0] = 4, a[1][1] = 5, a[1][2] = 6; 
-    a[2][0] = 7, a[2][1] = 8, a[2][2] = 9;
+    int **b = allocate_matrix(size_b, size_b);
+    b[0][0] = 1, b[0][1] = 2, b[0][2] = 3; 
+    b[1][0] = 4, b[1][1] = 5, b[1][2] = 6; 
+    b[2][0] = 7, b[2][1] = 8, b[2][2] = 9;
 
     equalize_matrices(&a, &b, &rows_a, &rows_b, &cols_a, &cols_b);
 
